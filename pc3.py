df3577ccc6b23d00b7a6ae9862121a5bb4a82a2d
Gaute Kolstad <gautekolstad@gmail.com>
2017-06-11 20:42:39 +0200

    updated challenge 3

--- pc3.py
+++ pc3.py
@@ -2,29 +2,26 @@ from collections import defaultdict
 
 d_char = defaultdict(str)
 
-f = open('input_pc3', 'r')
+text = ''.join([line.rstrip() for line in open('input_pc3')])
 
-for line in f:
-    for i in line:
-        if i in d_char:
-            d_char[i] += 1
-        else:
-            d_char[i] = 1
+for i in text:
+    if i in d_char:
+        d_char[i] += 1
+    else:
+        d_char[i] = 1
 
 chars = ''
 for key, value in d_char.iteritems():
-    if value < 4:
+    if value < 10:
         chars += key
 
 print chars
 word = ''
-f = open('input_pc3', 'r')
-for line in f:
-    for i in line:
-        if i in chars:
-            word += i
+
+for i in text:
+    if i in chars:
+        word += i
 
 print word
-f.close()
 
 # Output: http://www.pythonchallenge.com/pc/def/equality.html
